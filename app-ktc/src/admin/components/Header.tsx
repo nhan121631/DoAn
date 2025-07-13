65fdeb16401e7d2b74364f418903b385f384d156
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-13 16:44:03 +0700

    refactor: remove React import as it is not used

--- app-ktc/src/admin/components/Header.tsx
+++ app-ktc/src/admin/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 import { ThemeContext } from "../context/ThemeContext";
 
