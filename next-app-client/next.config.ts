2c0a12a1ed501c74c4f7d8110213d0bd0044625e
Trungg03 <vohuynhtrung1703@gmail.com>
2025-07-22 13:07:15 +0700

    ddđ

--- next-app-client/next.config.ts
+++ next-app-client/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
     domains: ["antimatter.vn", "cdn.luatminhkhue.vn"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 export default nextConfig;
