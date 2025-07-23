fe71549c788280fbb588fbe0fab067a5163014da
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-23 09:47:16 +0700

    feat: enhance UsersPage layout with responsive design and integrate RightSidebar

--- next-app-client/src/app/users/page.tsx
+++ next-app-client/src/app/users/page.tsx
@@ -1,3 +1,5 @@
+import FeaturedListingsCard from "../landlord/components/room-detail/InfoCardAndFeatured/FeaturedListingsCard";
+import RightSidebar from "../landlord/components/room-detail/RightSidebar";
 import CardFilter from "./components/Filter/CardFilter";
 import RoomCard from "./components/rooms/RoomCard";
 import RoomVipCard from "./components/rooms/RoomVipCard";
@@ -44,8 +46,8 @@ const data = {
 
 export default function UsersPage() {
   return (
-    <div className="flex justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col items-center justify-center bg-gray-100 p-4 gap-4">
+    <div className="flex flex-col lg:flex-row justify-center min-h-screen bg-gray-100 gap-0 lg:gap-x-2">
+      <div className="flex flex-col bg-gray-100 p-4 gap-4 max-w-7xl my-8 px-4 lg:px-0 w-full lg:w-auto">
         <h1 className="text-2xl font-bold text-left w-full">
           Vietnam’s No.1 Rental Room Platform
         </h1>
@@ -59,8 +61,11 @@ export default function UsersPage() {
         <RoomVipCard room={data} />
         <RoomCard room={data} isForSale={true} isFeatured={false} />
       </div>
-      <div className="flex flex-col  p-4 gap-4">
-        <CardFilter />
+      <div className="flex flex-col p-4 gap-4 w-full lg:w-1/3">
+        <div className="hidden lg:block">
+          <CardFilter />
+        </div>
+        <FeaturedListingsCard />
       </div>
     </div>
   );
