d57166bf1d34f1b4b72bf3708015cfc95910fc46
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-07-26 16:16:41 +0700

    cm

--- next-app-client/src/app/users/components/RightSidebar.tsx
+++ next-app-client/src/app/users/components/RightSidebar.tsx
@@ -6,7 +6,7 @@ import FeaturedListingsCard from './InfoCardAndFeatured/FeaturedListingsCard';
 
 export default function RightSidebar() {
   return (
-    <div className="w-full lg:w-[350px] flex flex-col ">
+    <div className="w-full lg:w-[350px] flex flex-col mt-15">
       <UserInfoCard />
       <FeaturedListingsCard />
     </div>
