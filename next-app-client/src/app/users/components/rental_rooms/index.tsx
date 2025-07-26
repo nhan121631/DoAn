5a90ca6965ac06229219bba72b175d1678889ceb
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-26 14:20:53 +0700

    Add Rentroom SVG and implement Why Choose Us section with contact form and map

--- next-app-client/src/app/users/components/rental_rooms/index.tsx
+++ next-app-client/src/app/users/components/rental_rooms/index.tsx
@@ -73,7 +73,7 @@ export default function RentalRooms() {
         </div>
       </div>
       <div className="flex flex-col gap-4 my-6 px-4 w-full max-w-7xl items-center justify-center">
-        <h3 className="text-xl font-semibold text-center w-full">
+        <h3 className="text-2xl font-semibold text-center w-full">
           Featured Listings
         </h3>
         <h5 className="text-md mb-3 font-normal text-center w-full">
