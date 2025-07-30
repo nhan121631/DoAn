980b9991f07d69f74b7b9705ace4f22062a44536
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-07-30 11:16:51 +0700

    fix(UserRoleRepository): change userId type from Long to UUID for better consistency

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/entities/PostType.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/entities/PostType.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/repositories/role/UserRoleRepository.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/repositories/role/UserRoleRepository.java
@@ -1,12 +1,13 @@
 package com.ants.ktc.ants_ktc.repositories.role;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ants.ktc.ants_ktc.entities.role.UserRole;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    List<UserRole> findByUserId(Long userId);
+    List<UserRole> findByUserId(UUID userId);
 
 }
