5b472c18b2ec5341f3616211a31974ec6551d8b9
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-30 11:15:21 +0700

    feat(entities): enhance entity definitions with nullable constraints and add Convenients entity

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/entities/Transactions.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/entities/Transactions.java
@@ -1,20 +1,28 @@
 package com.ants.ktc.ants_ktc.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "transactions")
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Transactions extends BaseEntity {
+    @Column(name = "amount", nullable = false)
     private Double amount;
+
+    @Column(name = "transaction_type", nullable = false)
     private int transactionType;
+
+    @Column(name = "status", nullable = false)
     private int status;
+
+    @Column(name = "description", length = 500)
     private String description;
 
     @ManyToOne
