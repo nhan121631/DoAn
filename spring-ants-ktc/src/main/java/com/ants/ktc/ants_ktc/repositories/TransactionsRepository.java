8331740a26529cf250f4266d0e8c001a0bba52ed
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-30 11:21:24 +0700

    refactor(transactions): change primary key type from Long to UUID in TransactionsRepository

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/repositories/TransactionsRepository.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/repositories/TransactionsRepository.java
@@ -1,10 +1,11 @@
 package com.ants.ktc.ants_ktc.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ants.ktc.ants_ktc.entities.Transactions;
 
-public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
-
+public interface TransactionsRepository extends JpaRepository<Transactions, UUID> {
 
 }
