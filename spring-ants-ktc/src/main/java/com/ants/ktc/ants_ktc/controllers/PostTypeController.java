ed2e09e10d61aa01fe12d21958be77345ba7b18c
Nhan121631 <phamphunhanpham8@gmail.com>
2025-08-02 10:54:59 +0700

    refactor(entities): remove unused imports and clean up entity classes; delete UserRoleRepository

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/controllers/PostTypeController.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/controllers/PostTypeController.java
@@ -1,11 +1,9 @@
 package com.ants.ktc.ants_ktc.controllers;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/services/PostTypeService.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/services/PostTypeService.java
@@ -1,6 +1,5 @@
 package com.ants.ktc.ants_ktc.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
