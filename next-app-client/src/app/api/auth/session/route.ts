d369414fe1563c34f4b2ae4823c6de3daa72f115
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-08-04 09:06:57 +0700

    refactor: remove unused session route code

--- next-app-client/src/app/api/auth/session/route.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
-
-export async function GET(request: Request) {
-  const session = await getServerSession(authOptions);
-  return Response.json(session ?? {});
-}
