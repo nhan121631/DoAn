a272492210ff6a2aca35915363b3f1e4f9d32bd0
Nhan121631 <phamphunhanpham8@gmail.com>
2025-08-08 18:21:22 +0700

    fix: Update API URL to switch to local development and adjust CORS configuration to disallow credentials

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/config/SecurityConfig.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/config/SecurityConfig.java
@@ -64,10 +64,10 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("*")); // Chấp nhận mọi origin
+                configuration.setAllowedOrigins(List.of("*"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                 configuration.setAllowedHeaders(List.of("*"));
-                configuration.setAllowCredentials(true);
+                configuration.setAllowCredentials(false); // Không cho gửi cookie/token
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                 source.registerCorsConfiguration("/**", configuration);
                 return source;
