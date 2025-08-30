9f2b2f1d8822b14e3d68c1d647093e6664d6c07e
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-08-30 14:53:25 +0700

    feat: Enhance validation in Booking and Room controllers; add tenant count check in BookingService

--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/dtos/room/RoomUpdateExpireDateRequestDto.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/dtos/room/RoomUpdateExpireDateRequestDto.java
@@ -3,6 +3,8 @@ package com.ants.ktc.ants_ktc.dtos.room;
 import java.util.Date;
 import java.util.UUID;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +18,13 @@ import lombok.NoArgsConstructor;
 public class RoomUpdateExpireDateRequestDto {
     @NotNull(message = "Room ID is required.")
     UUID roomId;
+
     @NotNull(message = "Post start date is required.")
+    @FutureOrPresent(message = "Post start date must be today or in the future.")
     Date postStartDate;
+    
     @NotNull(message = "Post end date is required.")
+    @Future(message = "Post end date must be in the future.")
     Date postEndDate;
     private UUID typepostId;
 
