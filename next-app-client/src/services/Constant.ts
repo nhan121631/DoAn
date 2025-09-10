aedd3646251a7245550b5f7885f942acf44f283c
Nhan121631 <phamphunhanpham8@gmail.com>
2025-09-10 14:32:44 +0700

    fix: Update API_URL export to use production URL and comment out local development URL

--- next-app-client/src/services/Constant.ts
+++ next-app-client/src/services/Constant.ts
@@ -1,8 +1,8 @@
-export const API_URL = "http://localhost:3333/api";
+// export const API_URL = "http://localhost:3333/api";
 // export const URL_IMAGE = "http://localhost:3333";
 export const URL_IMAGE = "https://res.cloudinary.com";
 // export const URL_PPYTHON = "http://localhost:5000";
 
-// export const API_URL = "https://rentalroom-51g4.onrender.com/api";
+export const API_URL = "https://rentalroom-51g4.onrender.com/api";
 // export const URL_IMAGE = "https://rentalroom-51g4.onrender.com";
 export const URL_PPYTHON = "https://ants-chatbot.onrender.com"
