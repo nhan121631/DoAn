f42c47abc8c2c53fedda878013b627014d510cce
Nhan121631 <phamphunhanpham8@gmail.com>
2025-09-22 10:45:41 +0700

    fix: Add target configuration to next.config.ts for serverless deployment

--- next-app-client/next.config.ts
+++ next-app-client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     // output: 'export',
+    target: 'serverless', // add this line
     images: {
     // domains: ["antimatter.vn", "cdn.luatminhkhue.vn"],
     remotePatterns: [
