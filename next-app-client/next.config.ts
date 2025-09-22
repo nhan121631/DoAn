02703b6e930091df53bb176ae086b6b234165225
Nhan121631 <phamphunhanpham8@gmail.com>
2025-09-22 10:41:04 +0700

    fix: Comment out output option in next.config.ts for production readiness

--- next-app-client/next.config.ts
+++ next-app-client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    output: 'export',
+    // output: 'export',
     images: {
     // domains: ["antimatter.vn", "cdn.luatminhkhue.vn"],
     remotePatterns: [
