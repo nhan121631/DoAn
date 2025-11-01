937ae358f3b5eacb878f22b8156d3af7c28625ef
Nhan121631 <phamphunhanpham8@gmail.com>
2025-11-01 14:02:45 +0700

    feat: Implement JWT session management with automatic logout and token validation

--- next-app-client/src/app/layout.tsx
+++ next-app-client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./components/providers/authProviders";
+import SessionWatcher from "@/components/SessionWatcher";
 import FavoriteInitializer from "./user-dashboard/components/favorited-rooms/FavoriteInitializer";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
         <NextAuthProvider>
+          <SessionWatcher />
           <AntdRegistry>
             <FavoriteInitializer />
             {children}
