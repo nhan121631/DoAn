cc85c52d242a2aa3f2d80f7c3d47add1e7cbc519
Nhan121631 <phamphunhanpham8@gmail.com>
2025-11-26 14:52:14 +0700

    Update application.properties to enable MySQL datasource configuration and comment out local Docker settings

--- next-app-client/src/services/Constant.ts
+++ next-app-client/src/services/Constant.ts
@@ -1,9 +1,9 @@
-// export const API_URL = "http://localhost:3333/api";
+export const API_URL = "http://localhost:3333/api";
 // export const URL_IMAGE = "http://localhost:3333";
 export const URL_IMAGE = "https://res.cloudinary.com";
 export const URL_PPYTHON = "http://localhost:5001";
 
 // export const API_URL = "http://138.68.234.160:3333/api";
-export const API_URL = "https://rentalroom-51g4.onrender.com/api";
+// export const API_URL = "https://rentalroom-51g4.onrender.com/api";
 // export const URL_IMAGE = "https://rentalroom-51g4.onrender.com";
 // export const URL_PPYTHON = "https://ants-chatbot.onrender.com"
