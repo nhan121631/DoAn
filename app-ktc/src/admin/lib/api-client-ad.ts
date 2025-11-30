1c4fc3dc6eadc43f82b083d33eefed7a9281faa0
Nhan121631 <phamphunhanpham8@gmail.com>
2025-11-30 16:32:58 +0700

    fix: update database configuration in application.properties and adjust API URL comments in api-client-ad.ts

--- app-ktc/src/admin/lib/api-client-ad.ts
+++ app-ktc/src/admin/lib/api-client-ad.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Axios, { type InternalAxiosRequestConfig } from 'axios';
 
-const URL = 'http://localhost:3333/api'; 
-// const URL = 'https://jones-hold-mold-cohen.trycloudflare.com/api';
+// const URL = 'http://localhost:3333/api'; 
+const URL = 'https://jones-hold-mold-cohen.trycloudflare.com/api';
 
 const apiClient = Axios.create({
   baseURL: URL,
