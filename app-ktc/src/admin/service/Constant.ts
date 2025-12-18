c0dae07e0a1b74134a7f8579f69c8f8cf009eb9c
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-12-18 13:47:44 +0700

    fix: Update API_URL and URL_IMAGE constants for consistency in configuration

--- app-ktc/src/admin/service/Constant.ts
+++ app-ktc/src/admin/service/Constant.ts
@@ -1,4 +1,4 @@
 export const API_URL = "http://localhost:3333/api"
 // export const API_URL = "https://rentalroom-51g4.onrender.com/api"
 // export const API_URL = "https://jones-hold-mold-cohen.trycloudflare.com/api"
-export const IMAGE_URL = "https://res.cloudinary.com"
+export const URL_IMAGE = "https://res.cloudinary.com"
