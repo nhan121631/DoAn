a4d1c0f7ca1187c9cb44229ce2de70f877af7090
khoinv21it <99341144+khoinv21it@users.noreply.github.com>
2025-12-18 13:23:14 +0700

    fix: Update API_URL constant for axios client configuration

--- app-ktc/src/admin/service/Constant.ts
+++ app-ktc/src/admin/service/Constant.ts
@@ -1,3 +1,4 @@
-export const URL_API = "http://localhost:3333/api"
-// export const URL_API = "https://rentalroom-51g4.onrender.com/api"
-export const URL_IMAGE = "https://res.cloudinary.com"
+export const API_URL = "http://localhost:3333/api"
+// export const API_URL = "https://rentalroom-51g4.onrender.com/api"
+// export const API_URL = "https://jones-hold-mold-cohen.trycloudflare.com/api"
+export const IMAGE_URL = "https://res.cloudinary.com"
